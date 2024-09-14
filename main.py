9869ab62ee5118a42117d33127acec9f5e73d73b
arslan0007 <arsla.n7.cl@gmail.com>
2024-09-14 20:41:00 +0300

    Fetching related coin currency rate. Binding Docker to Google Cloud VM. You can use GCloud VM external IP address to fetch related data (http://34.46.64.231:8080/usd/). You can change usd to eur or gbp

---
+++ main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import requests
+import json
+import os
+
+
+app = FastAPI(
+    title="MyApp",
+    description="Hello API dev",
+    version="0.0.1"
+)
+
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+@app.get("/{currency}/")
+async def root(currency):
+    # getting an information with requests
+    response = requests.get(url)
+    # converting them into a json
+    response_json = response.json()
+    # fetching a specific area to find related currency
+    price_cur = response_json['bpi']
+    # convert them to lowercase
+    price_cur_lower = json.loads(json.dumps(price_cur).lower())
+    # fetching the related rate of currency
+    price_cur_rate = price_cur_lower[currency]['rate']
+    # sending the result
+    return {"message": price_cur_rate}
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = "favicon.ico"
+    file_path = os.path.join(app.root_path, "static", file_name)
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
+
+print("test bro")
